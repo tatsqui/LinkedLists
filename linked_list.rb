5128e81594172856b4516f0c1de1b44228def098
Tatiana Quintana <tatsqui@gmail.com>
2019-05-09 14:05:23 -0700

    added first five methods, tests pass

--- linked_list.rb
+++ linked_list.rb
@@ -8,23 +8,58 @@ class LinkedList
   end
 
   def add_first(value)
-
+    new_node = Node.new(value)
+  if !@head.nil?
+    new_node.next = @head
+  end
+  
+  @head = new_node
   end
 
   def get_first
-
+    if @head != nil
+      return @head.data
+    end
   end
 
   def length
-    return 0
+    current_node = @head
+    count = 0
+
+    while current_node != nil
+      count += 1
+      current_node = current_node.next
+    end
+
+    return count
   end
 
   def add_last(value)
+    new_node = Node.new(value)
 
+    node = @head
+
+    if @head != nil
+      until node.next == nil
+        node = node.next
+      end
+
+      node.next = new_node
+
+      return new_node
+    else
+      add_first(value)
+    end
   end
 
   def get_last
+    node = @head
 
+    until node.next == nil
+      node = node.next
+    end
+
+    return node.data
   end
 
   def get_at_index(index)
--- linked_list_spec.rb
+++ linked_list_spec.rb
@@ -51,7 +51,7 @@ describe LinkedList do
     end
   end
 
-  xdescribe "length" do
+  describe "length" do
     it "will return 0 for an empty list" do
       expect(@list.length).must_equal 0
     end
@@ -66,7 +66,7 @@ describe LinkedList do
     end
   end
 
-  xdescribe "addLast & getLast" do
+  describe "addLast & getLast" do
     it "will add to the front if the list is empty" do
       @list.add_last(1)
       expect(@list.get_first).must_equal 1
